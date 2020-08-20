e9ff0004baab8e518ebf0b9bca3406c04fc3bda8
codegangsk <kjhj0604@gmail.com>
2020-08-20 20:49:49 +0900

    Initial Commit

---
+++ 3PersonalityQuiz(project)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3PersonalityQuiz(project)
+//
+//  Created by Sophie Kim on 2020/08/20.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
